aed15e62107d812582c288b0c33ce4e2152b26bd
Sameer Faridi <s.faridi007@gmail.com>
2023-10-23 21:58:22 +0530

    day06 done

---
+++ 60_Days_challange/day06/binary_sort.js
@@ -0,0 +1,15 @@
+const search = (arr, t) => {
+  let LI = 0;
+  let RI = arr.length - 1;
+
+  while (LI < RI) {
+    let mid = LI + Math.floor((RI - LI + 1) / 2);
+    if (t < arr[mid]) RI = mid - 1;
+    else LI = mid;
+  }
+  return arr[LI] == t ? LI : -1;
+};
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let target = 9;
+console.log(search(arr, target));
