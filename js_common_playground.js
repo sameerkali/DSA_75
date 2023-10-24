c30cb5b3b51656b04cde32eb904a1208e5077fff
Sameer Faridi <s.faridi007@gmail.com>
2023-10-24 19:34:22 +0530

    practice this keyword in js

--- js_common_playground.js
+++ js_common_playground.js
@@ -5,7 +5,7 @@ const call = {
     console.log(`Hey, ${this.caller} just called.`);
   }
 };
-call.says(); // Hey, mom just called.
+// call.says(); // Hey, mom just called.
 
 // (B) Challenge #2
 const call2 = {
@@ -14,7 +14,7 @@ const call2 = {
     console.log(`Hey, ${this.caller} just called.`);
   }
 };
-call2.says(); // Hey, undefined just called.
+// call2.says(); // Hey, undefined just called.
 
 //Challenge #3
 const call3 = {
@@ -26,7 +26,7 @@ const call3 = {
 
 let newCall = call3.says;
 
-newCall(); // Hey, undefined just called.
+// newCall(); // Hey, undefined just called.
 
 //* explaination :
 //!  Here, we declare a new variable, newCall, and assign the says function inside the call object to newCall. And then we invoke newCall, which is a now simple function call.
@@ -35,20 +35,35 @@ newCall(); // Hey, undefined just called.
 
 //! As demonstrated in Challenge#2, since the global object does not have a caller property, you get "undefined" as a result.
 
-
-
-// Challenge #4 
+// Challenge #4
 function anotherCaller() {
-    console.log(`${this.caller} called, too!`);
+  console.log(`${this.caller} called, too!`);
+}
+
+const call4 = {
+  caller: "mom",
+  anotherCaller: anotherCaller,
+  says: function () {
+    console.log(`Hey, ${this.caller} just called.`);
   }
-  
-  const call4 = {
-    caller: "mom", 
-    anotherCaller: anotherCaller,
-    says: function() {
-      console.log(`Hey, ${this.caller} just called.`);
-    }
-  };
-  
-  let newCall2 = call4.anotherCaller;
-  newCall2()
+};
+
+let newCall2 = call4.anotherCaller;
+//   newCall2()
+
+/// #5
+
+//   function foo () {
+//     console.log("Simple function call");
+//     console.log(this === window);
+//   }
+//   foo();
+
+// #6
+
+
+const arr = new Array();
+
+for(let key in arr) {
+    console.log(`key: ${key} and the value: ${arr[key]}`);
+}
