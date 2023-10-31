535a7810af60950a3fbb92f87df6dac8f36772a9
Sameer Faridi <s.faridi007@gmail.com>
2023-10-31 06:44:42 +0530

    day14

--- 60_Days_challange/day11/firstBadVersion.js
+++ 60_Days_challange/day11/firstBadVersion.js
@@ -1,13 +1,13 @@
-var solution = function (isBadVersion: any) {
-    return function (n: number): number {
-        let low = 1;
-        let high = n;
+// var solution = function (isBadVersion) {
+//     return function (n):  {
+//         let low = 1;
+//         let high = n;
 
-        while (low <= high) {
-            const mid = Math.floor((low + high) / 2);
-            const ans = isBadVersion(mid);
-            ans ? high = mid - 1 : low = mid + 1;
-        }
-        return low;
-    };
-};
+//         while (low <= high) {
+//             const mid = Math.floor((low + high) / 2);
+//             const ans = isBadVersion(mid);
+//             ans ? high = mid - 1 : low = mid + 1;
+//         }
+//         return low;
+//     };
+// };
---
+++ 60_Days_challange/day14/longest_palendrome.js
@@ -0,0 +1,18 @@
+
+var longestPalindrome = function(s) {
+    let obj ={};
+    let answer=0;
+    for(let c of s){
+        if(!obj[c]){
+                obj[c]=1;
+        }
+        else
+             obj[c]++;
+    if(obj[c]%2===0){
+        answer += 2;
+    }
+    }
+  return s.length>answer ? answer+1 :answer;  
+};
+
+console.log(longestPalindrome("sameerfaridi"))
