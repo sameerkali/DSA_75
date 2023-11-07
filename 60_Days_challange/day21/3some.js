317ba128ffd9546cd024604042614a3455f1844d
Sameer Faridi <s.faridi007@gmail.com>
2023-11-07 21:38:33 +0530

    day21`

---
+++ 60_Days_challange/day21/3some.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    const res = [];
+    nums.sort((a, b) => a - b);
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i === 0 || nums[i] !== nums[i-1]) {
+            let lo = i + 1, hi = nums.length - 1, sum = 0 - nums[i];
+            while (lo < hi) {
+                if (nums[lo] + nums[hi] === sum) {
+                    res.push([nums[i], nums[lo], nums[hi]]);
+                    while (lo < hi && nums[lo] === nums[lo+1]) lo++;
+                    while (lo < hi && nums[hi] === nums[hi-1]) hi--;
+                    lo++;
+                    hi--;
+                } else if (nums[lo] + nums[hi] < sum) {
+                    lo++;
+                } else {
+                    hi--;
+                }
+            }
+        }
+    }
+    
+    return res;
+};
