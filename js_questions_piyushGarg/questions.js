e147f6d1300d027144a5dc1560e5fb1c2889ea47
Sameer Faridi <s.faridi007@gmail.com>
2023-12-01 21:53:55 +0530

    piyush garg questons

---
+++ js_questions_piyushGarg/questions.js
@@ -0,0 +1,29 @@
+//! Question 01
+// console.log("Value of Age is", age);
+// //let age = 20;
+// var age = 20;
+// console.log("Value of Age is", age);
+// answer hint - GEC
+
+//! Question 02
+// age = 100;
+// console.log("Value of Age is", age);
+// var age = 30;
+// // let age = 30; //error || also there is a temporal dead zone
+//! Question 03
+myFunc();
+var myFunc = function () {
+  console.log("first");
+};
+myFunc();
+function myFunc() {
+  console.log("secound");
+}
+myFunc();
+
+//! Question 04
+//! Question 05
+//! Question 06
+//! Question 07
+//! Question 08
+//! Question 09
