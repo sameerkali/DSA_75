5f0d7ac74873d2c2e7f30606943defd6a0b4f33f
Sameer Faridi <s.faridi007@gmail.com>
2023-12-05 17:33:24 +0530

    polyfill implimentation

---
+++ Polyfill/find.js
@@ -0,0 +1,8 @@
+Array.prototype.find = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return this[i];
+    }
+  }
+  return undefined;
+};
---
+++ Polyfill/map.js
@@ -0,0 +1,11 @@
+Array.prototype.sap = function (callback) { // i name sap because of testind code
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
+};
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const result = arr.sap((a) => a * 2);
+console.log(result);
