90f12711753afcbbaac48ea29b77a10c064a0e49
Sameer Faridi <s.faridi007@gmail.com>
2023-12-29 11:40:08 +0530

    DSA (Minimum Difficulty of a Job Schedule)

--- 60_Days_challange/day69/MinimumDifficultyofaJobSchedule.js
+++ 60_Days_challange/day69/MinimumDifficultyofaJobSchedule.js
@@ -4,22 +4,22 @@
  * @return {number}
  */
 const minDifficulty = (jobs, d) => {
-    const n = jobs.length;
-    if (n < d) return -1;
-        const dp = new Array(n + 1).fill(0);
+  const n = jobs.length;
+  if (n < d) return -1;
+  const dp = new Array(n + 1).fill(0);
 
-    for (let i = n - 1; i >= 0; i--)
-        dp[i] = jobs[i] > dp[i + 1] ? jobs[i] : dp[i + 1];
+  for (let i = n - 1; i >= 0; i--)
+    dp[i] = jobs[i] > dp[i + 1] ? jobs[i] : dp[i + 1];
 
-    for (let i = 2; i <= d; i++)
-        for (let j = 0; j <= n - i; j++) {
-            let max = 0;
-            dp[j] = Infinity;
-            for (let k = j; k <= n - i; k++) {
-                if (jobs[k] > max) max = jobs[k];
-                if (dp[j] > dp[k + 1] + max) dp[j] = dp[k + 1] + max;
-            }
-        }
+  for (let i = 2; i <= d; i++)
+    for (let j = 0; j <= n - i; j++) {
+      let max = 0;
+      dp[j] = Infinity;
+      for (let k = j; k <= n - i; k++) {
+        if (jobs[k] > max) max = jobs[k];
+        if (dp[j] > dp[k + 1] + max) dp[j] = dp[k + 1] + max;
+      }
+    }
 
-    return dp[0];
+  return dp[0];
 };
