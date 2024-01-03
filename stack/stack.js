2fa56fb84d6289703448bfb3717f08054fb348a6
Sameer Faridi <s.faridi007@gmail.com>
2024-01-03 10:51:28 +0530

    dsa

--- stack/stack.js
+++ stack/stack.js
@@ -1,4 +1,31 @@
-console.log("hellp stack")
-console.log(first)
-console.log(first)
-console.log(first)
+/**
+ * @param {string[]} bank
+ * @return {number}
+ */
+var numberOfBeams = function (bank) {
+
+
+    const totalLasers = []; // totalLasers in each row
+    let totalBeams = 0;
+
+    for (let i = 0; i < bank.length; i++) {
+        let currentLasers = 0; // laser device in current row
+
+        for (let j = 0; j < bank[i].length; j++) {
+            if (bank[i][j] === '1') {
+                currentLasers += 1
+            }
+        }
+
+        if (currentLasers) {
+            totalLasers.push(currentLasers);
+        }
+    }
+
+    for (let i = 0; i + 1 < totalLasers.length; i++) {
+        totalBeams += totalLasers[i] * totalLasers[i + 1];
+    }
+
+    return totalBeams;
+
+};
