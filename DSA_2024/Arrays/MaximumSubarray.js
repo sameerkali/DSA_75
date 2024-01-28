de455c737862055e46d9ab89ca7020555ff8e010
Sameer Faridi <s.faridi007@gmail.com>
2024-01-28 17:30:25 +0530

    fully optimized solution of this question

--- DSA_2024/Arrays/MaximumSubarray.js
+++ DSA_2024/Arrays/MaximumSubarray.js
@@ -1,12 +1,25 @@
+//! unoptimized
+// const maxSubArray = (n) => {
+//   let max = Number.NEGATIVE_INFINITY,
+//     len = n.length;
+//   for (let i = 0; i < len; i++) {
+//     let sum = 0;
+//     for (let j = i; j < len; j++) {
+//       sum += n[j];
+//       max = Math.max(sum, max);
+//     }
+//   }
+//   return max;
+// };
+
+//*optimized
 const maxSubArray = (n) => {
-  let max = Number.NEGATIVE_INFINITY,
-    len = n.length;
-  for (let i = 0; i < len; i++) {
-    let sum = 0;
-    for (let j = i; j < len; j++) {
-      sum += n[j];
-      max = Math.max(sum, max);
-    }
+  let max = n[0];
+  let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    sum += n[i];
+    if (sum > max) max = sum;
+    if (sum < 0) sum = 0;
   }
   return max;
 };
