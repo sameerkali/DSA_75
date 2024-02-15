f877f9ac6ef41375711b5cbe276b16bc2cae0f15
Sameer Faridi <s.faridi007@gmail.com>
2024-02-15 23:38:21 +0530

    getMinMax DSA

--- DSA_2024/Arrays/min_max copy.js
+++ DSA_2024/Arrays/min_max copy.js
@@ -1,16 +1,16 @@
-// const getMinMax = (arr, n) => {
-//   let min = Number.MAX_VALUE,
-//     max = Number.MIN_VALUE;
-//   for (let i = 0; i < n - 1; i++) {
-//     if (arr[i] < min) min = arr[i];
-//     if (arr[i] > min) max = arr[i];
-//   }
-//   return [min, max];
-// };
+const getMinMax = (arr, n) => {
+  let min = Number.MAX_VALUE,
+    max = Number.MIN_VALUE;
+  for (let i = 0; i < n - 1; i++) {
+    if (arr[i] < min) min = arr[i];
+    if (arr[i] > min) max = arr[i];
+  }
+  return [min, max];
+};
 
-// let arr = [3, 2, 1, 56, 1000, 167],
-//   n = 6;
-// console.log(getMinMax(arr, n));
+let arr = [3, 2, 1, 56, 1000, 167],
+  n = 6;
+console.log(getMinMax(arr, n));
 
 
 
