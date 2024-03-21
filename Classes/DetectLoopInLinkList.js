5b45f9a43e8df2a64d40594bcdb9d20a515ad450
Sameer Faridi <s.faridi007@gmail.com>
2024-03-21 12:21:41 +0530

    add and delete data from linkList

--- Classes/DetectLoopInLinkList.js
+++ Classes/DetectLoopInLinkList.js
@@ -14,27 +14,23 @@ class ListNode {
       fast = fast.next.next;
   
       if (slow === fast) {
-        return true; // Cycle detected
+        return true; 
       }
     }
   
-    return false; // No cycle detected
+    return false; 
   }
-  
-// Create nodes for the linked list
+
 const node1 = new ListNode(1);
 const node2 = new ListNode(2);
 const node3 = new ListNode(3);
 const node4 = new ListNode(4);
 
-// Construct the linked list with a cycle
 node1.next = node1;
 node2.next = node2;
 node3.next = node3;
-node4.next = node4; // Cycle back to node2
+node4.next = node4;
 
-// Check if the linked list has a cycle
 const hasCycleResult = hasCycle(node1);
 
-// Print the result in the console
 console.log("Does the linked list have a cycle?", hasCycleResult);
