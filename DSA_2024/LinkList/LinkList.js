d9c6ad7621ce1d0ecf2ea607727ada1ee668405e
Sameer Faridi <s.faridi007@gmail.com>
2024-03-24 15:08:06 +0530

    loop In LinkedList in js DSA

--- DSA_2024/LinkList/LinkList.js
+++ DSA_2024/LinkList/LinkList.js
@@ -1,23 +1,23 @@
-class Node {
-    constructor(data) {
-      this.data = data;
-      this.next = null;
+  class Node {
+      constructor(data) {
+        this.data = data;
+        this.next = null;
+      }
     }
+
+  class linklist {
+      constructor(){
+          this.head = null;
+      }
+
+      addFirst(data){
+          const newNode = new Node(data);
+          newNode.next = this.head;
+          this.head = newNode;
+      }
   }
 
-class linklist {
-    constructor(){
-        this.head = null;
-    }
-
-    addFirst(data){
-        const newNode = new Node(data);
-        newNode.next = this.head;
-        this.head = newNode;
-    }
-}
-
-const linklist2 = new linklist();
-linklist.addFirst(2);
-linklist.addFirst(3);
-console.log(linklist2)
+  // const linklist2 = new linklist();
+  // linklist.addFirst(2);
+  // linklist.addFirst(3);
+  // console.log(linklist2)
