45c4b3c2281de7a91f613aa2f7c74289c01b1e7f
Sameer Faridi <s.faridi007@gmail.com>
2024-03-28 23:48:54 +0530

    LinkedList functions: addFirst(), displayAll()

--- DSA_2024/LinkList/LinkList.js
+++ DSA_2024/LinkList/LinkList.js
@@ -1,62 +1,69 @@
-  // class Node {
-  //     constructor(data) {
-  //       this.data = data;
-  //       this.next = null;
-  //     }
-  //   }
+// class Node {
+//     constructor(data) {
+//       this.data = data;
+//       this.next = null;
+//     }
+//   }
 
-  // class linklist {
-  //     constructor(){
-  //         this.head = null;
-  //     }
+// class linklist {
+//     constructor(){
+//         this.head = null;
+//     }
 
-  //     addFirst(data){
-  //       if(this.head == null){
-  //         this.head = new Node(data);
-  //       }else{
-  //         let temp = new Node(data);
-  //         temp.next = this.head;
-  //         this.head = temp;
+//     addFirst(data){
+//       if(this.head == null){
+//         this.head = new Node(data);
+//       }else{
+//         let temp = new Node(data);
+//         temp.next = this.head;
+//         this.head = temp;
 
-  //       }
-  //       console.log(this.head)
-  //     }
-  //     display(){}
-  // }
+//       }
+//       console.log(this.head)
+//     }
+//     display(){}
+// }
 
-
-  // let a = new linklist();
-  // a.addFirst(4)
-  // a.addFirst(3)
-  // a.addFirst(2)
-  // a.addFirst(1)
-
-
-
-
+// let a = new linklist();
+// a.addFirst(4)
+// a.addFirst(3)
+// a.addFirst(2)
+// a.addFirst(1)
 
 class Node {
-  constructor(data){
-    this.data= data
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
 }
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  addFirst(data) {
+    if (this.head == null) {
+      this.head = new Node(data);
+    } else {
+      let temp = new Node(data);
+      temp.next = this.head;
+      this.head = temp;
+    }
+  }
 
+  displayAll() {
+    while (this.head != null) {
+      console.log(this.head.data);
+      this.head = this.head.next;
+    }
+  }
+}
 
+let a = new LinkedList();
 
+a.addFirst(2);
+a.addFirst(4);
+a.addFirst(3);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+a.displayAll();
