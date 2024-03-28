d0380fde176bd717105fba88aabde96d1124f484
Sameer Faridi <s.faridi007@gmail.com>
2024-03-28 16:41:17 +0530

    linkedlist  try to impliment by my own

--- DSA_2024/LinkList/LinkList.js
+++ DSA_2024/LinkList/LinkList.js
@@ -1,19 +1,62 @@
-  class Node {
-      constructor(data) {
-        this.data = data;
-        this.next = null;
-      }
-    }
+  // class Node {
+  //     constructor(data) {
+  //       this.data = data;
+  //       this.next = null;
+  //     }
+  //   }
 
-  class linklist {
-      constructor(){
-          this.head = null;
-      }
+  // class linklist {
+  //     constructor(){
+  //         this.head = null;
+  //     }
 
-      addFirst(data){
-          const newNode = new Node(data);
-          newNode.next = this.head;
-          this.head = newNode;
-      }
+  //     addFirst(data){
+  //       if(this.head == null){
+  //         this.head = new Node(data);
+  //       }else{
+  //         let temp = new Node(data);
+  //         temp.next = this.head;
+  //         this.head = temp;
+
+  //       }
+  //       console.log(this.head)
+  //     }
+  //     display(){}
+  // }
+
+
+  // let a = new linklist();
+  // a.addFirst(4)
+  // a.addFirst(3)
+  // a.addFirst(2)
+  // a.addFirst(1)
+
+
+
+
+
+class Node {
+  constructor(data){
+    this.data= data
   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
