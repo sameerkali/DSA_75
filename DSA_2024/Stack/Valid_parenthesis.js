34fa9c0d9f64d605459291af27b1f968f8545a37
Sameer Faridi <s.faridi007@gmail.com>
2024-04-18 12:24:01 +0530

    valid parenthesis problem in stack DSA

---
+++ DSA_2024/Stack/Valid_parenthesis.js
@@ -0,0 +1,14 @@
+// Ques 2 : Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+// determine if the input string is valid.
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+// Input: "()"       ----->>>>> Output: true
+// Input: "()[]{}"   ----->>>>> Output: true
+// Input: "(]"       ----->>>>> Output: false
+
+const isValid = (s) =>{
+
+    
+
+}
