307c6d2db598cdc33a3cac05b7193de56f8c811e
Sameer Faridi <s.faridi007@gmail.com>
2024-04-19 22:50:21 +0530

    promise revise

--- promise.js
+++ promise.js
@@ -6,49 +6,66 @@ const promise1 = new Promise((resolve, reject) => {
 });
 const promise2 = new Promise((resolve, reject) => {
   //   setTimeout(() => resolve("p2 is sucess"), 1000);
-  setTimeout(() => reject("p2 is fail"), 600);
+  setTimeout(() => resolve("p2 is pass"), 6);
 });
 const promise3 = new Promise((resolve, reject) => {
   //   setTimeout(() => resolve("p3 is sucess"), 2000);
-  setTimeout(() => reject("p3 is fail"), 500);
+  setTimeout(() => reject("p3 is fail"), 200);
+});
+const promise4 = new Promise((resolve, reject) => {
+  //   setTimeout(() => resolve("p4 is sucess"), 2000);
+  setTimeout(() => reject("p4 is fail"), 30);
+});
+const promise5 = new Promise((resolve, reject) => {
+  //   setTimeout(() => resolve("p5 is sucess"), 2000);
+  setTimeout(() => resolve("p5 is pass"), 100);
+});
+const promise6 = new Promise((resolve, reject) => {
+  //   setTimeout(() => resolve("p6 is sucess"), 2000);
+  setTimeout(() => reject("p6 is fail"), 1100);
 });
 
 //* promise.all()
 
-// Promise.all([promise1, promise2, promise3])
+// Promise.all([promise1, promise2, promise3,promise4, promise5, promise6])
 //   .then((res) => {
 //     console.log(res);
 //   })
 //   .catch((err) => {
 //     console.log(err);
 //   });
+  /* first fail one */
 
 //* promise.allSetteld()
 
-// Promise.allSettled([promise1, promise2, promise3])
+// Promise.allSettled([promise1, promise2, promise3,promise4, promise5, promise6])
 //   .then((res) => {
 //     console.log(res);
 //   })
 //   .catch((err) => {
 //     console.log(err);
 //   });
+  /* tell the status of all after executing all */
 
 //* promise.race()
 
-// Promise.race([promise1, promise2, promise3])
+// Promise.race([promise1, promise2, promise3,promise4, promise5, promise6])
 //   .then((res) => {
 //     console.log(res);
 //   })
 //   .catch((err) => {
 //     console.log(err);
 //   });
+  /* tell the fastest one no metter what is the response */
 
 //* promise.any()
 
-Promise.any([promise1, promise2, promise3])
+Promise.any([promise1, promise2, promise3,promise4, promise5, promise6])
   .then((res) => {
     console.log(res);
   })
   .catch((err) => {
     console.log(err);
   });
+
+  /* return first sucess promise */
