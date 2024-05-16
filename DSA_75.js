7cf4eb71ee084f726b04d9f1c93f4d6cbbe2532b
Sameer Faridi <s.faridi007@gmail.com>
2024-05-16 17:28:16 +0530

    Evaluate Boolean Binary Tree

--- DSA_75.js
+++ DSA_75.js
@@ -946,3 +946,33 @@ const countVowels2 = (vowels) => {
   return vowelsLength ? vowelsLength.length : 0;
 };
 // console.log(countVowels2(vowels2));
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var evaluateTree = function(root) {
+  function helper(node) {
+      if (node.val === 0 || node.val === 1) {
+          return node.val === 1;
+      } else if (node.val === 2) {
+          return helper(node.left) || helper(node.right);
+      } else if (node.val === 3) {
+          return helper(node.left) && helper(node.right);
+      }
+      return false;
+  }
+  
+  return helper(root);
+};
