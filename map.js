8cc914921d988395b6fe564a5b03bdf2547fea0e
sameerkali <s.faridi007@gmail.com>
2024-07-11 14:34:54 +0530

    map filter reduce

---
+++ map.js
@@ -0,0 +1,14 @@
+console.log(".......................................................")
+
+
+const arr = [1,2,3,4,5,6,7,8,9,10]
+const jk = arr.map( (a) => a*2)
+const kl = arr.filter( (a) => a%2===0)
+// console.log(jk)
+// console.log(kl)
+
+const reduce = arr.reduce((a, c) => a + c)
+console.log(reduce)
+
+console.log(".......................................................")
+
