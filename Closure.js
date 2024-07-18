43fe7474d66f5bd1c82f89258de6e01331ed4a5e
sameerkali <s.faridi007@gmail.com>
2024-07-18 09:37:32 +0530

    curring and closures practice

--- Closure.js
+++ Closure.js
@@ -1,103 +1,134 @@
-// Q1 
+// Q1
 function outer() {
-    let outerVar = 10;
-     function inner() {
-       console.log(outerVar);
-     }
-    return inner;
+  let outerVar = 10;
+  function inner() {
+    console.log(outerVar);
   }
-  const closureFunc = outer();
-  // closureFunc();
-
-
+  return inner;
+}
+const closureFunc = outer();
+// closureFunc();
 
 //Q2
 function greet(name) {
-    return function() {
-        console.log(`Hello, ${name}!`);
-    };
+  return function () {
+    console.log(`Hello, ${name}!`);
+  };
 }
-const sayHello = greet('Alice');
+const sayHello = greet("Alice");
 // sayHello();
-
 
 //Q3
 function counter() {
-    let count = 0;
-  
-    return {
-      increment: function() {
-        count++;
-        console.log(count);
-      },
-      decrement: function() {
-        count--;
-        console.log(count);
-      },
-      getCount: function() {
-        return count;
-      }
-    };
-  }
-  
-  // const counterInstance = counter();
-  // counterInstance.increment();
-  // counterInstance.increment();
-  // counterInstance.decrement();
-  // console.log(counterInstance.getCount());
-  
+  let count = 0;
 
+  return {
+    increment: function () {
+      count++;
+      console.log(count);
+    },
+    decrement: function () {
+      count--;
+      console.log(count);
+    },
+    getCount: function () {
+      return count;
+    },
+  };
+}
 
+// const counterInstance = counter();
+// counterInstance.increment();
+// counterInstance.increment();
+// counterInstance.decrement();
+// console.log(counterInstance.getCount());
 
 //Q4
 function delayedGreeting(name) {
-    setTimeout(function() {
-      console.log(`Hello, ${name}!`);
-    }, 1000);
-  }
-  
-  // delayedGreeting('Bob');
-  // console.log('Greetings sent!');
+  setTimeout(function () {
+    console.log(`Hello, ${name}!`);
+  }, 1000);
+}
 
+// delayedGreeting('Bob');
+// console.log('Greetings sent!');
 
-  
 //Q5
 function createCounter() {
-    let count = 0;
-    const counters = [];
-  
-    for (let i = 0; i < 3; i++) {
-      counters.push(function() {
-        console.log(count++);
-      });
-    }
-  
-    return counters;
+  let count = 0;
+  const counters = [];
+
+  for (let i = 0; i < 3; i++) {
+    counters.push(function () {
+      console.log(count++);
+    });
   }
-  
+
+  return counters;
+}
+
 //   const counters = createCounter();
 //   counters[0]();
 //   counters[1]();
 //   counters[2]();
-  
+
 //Q6
-
-
-
-
 
 //  x= 4
 //  console.log(x);
 // let x
-// var x 
+// var x
 // const x
 
+// const a = 10
+// console.log(a+1)
+// const fuinc = () => {
+//   console.log(a+2)
+// }
+// console.log(a+3)
 
-const a = 10 
-console.log(a+1)
-const fuinc = () => {
-  console.log(a+2)
+// fuinc()
+
+function closures () {
+  const a = 10
+  function inside() {
+    console.log(a)
+  }
+  inside()
 }
-console.log(a+3)
 
-fuinc()
+// closures()
+
+const closures2 = () => {
+  const a = 10;
+  const inside = () => {
+    console.log(a);
+    const inside2 = () => {
+      console.log(a + 1);
+    };
+    inside2();
+  };
+  inside();
+};
+
+// closures2();
+
+
+
+
+//curring
+
+const a = (x) => {
+  return (y) => {
+    return (z) => {
+      console.log(x+y+z)
+    };
+  };
+};
+//we can call this way
+// const and = a(2)
+// const anb = and(3)
+// let zzz = anb(5)
+
+//or using curring 
+// a(1)(2)(3)
