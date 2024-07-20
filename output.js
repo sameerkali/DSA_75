24f22d31d1f5306535544d00641b1e41e73221c2
Sameer Faridi <s.faridi007@gmail.com>
2024-07-20 11:34:21 +0530

    iffe

--- output.js
+++ output.js
@@ -1,6 +1,7 @@
+//! EASY
+
 // Question - 1
 // console.log(5 + "3");
-
 
 // // Question - 2
 // const arr = [1, 2, 3];
@@ -40,4 +41,73 @@
 // console.log(`Hello, ${name}!`);
 
 // // Question - 10
-console.log([1, 2, 3].indexOf(2));
+// console.log([1, 2, 3].indexOf(2));
+
+//! medium
+
+// Question - 1
+let x = function() {
+    return 10;
+}();
+console.log(x);
+
+// Question - 2
+// const obj = {
+//     name: "Alice",
+//     greet: function() {
+//       console.log(`Hello, ${this.name}!`);
+//     }
+//   };
+//   const greet = obj.greet;
+//   greet();
+  
+// Question - 3
+// let arr = [1, 2, 3, 4, 5];
+// let newArr = arr.filter(num => num > 2);
+// console.log(newArr);
+
+// Question - 4
+// function add(a, b) {
+//     return a + b;
+//   }
+//   console.log(add(2, '3'));
+  
+// Question - 5
+// let obj = { a: 1, b: 2 };
+// let newObj = { ...obj, c: 3 };
+// console.log(newObj);
+
+// Question - 6
+// const nums = [1, 2, 3, 4];
+// const result = nums.map(num => num * 2).reduce((a, b) => a + b);
+// console.log(result);
+
+// Question - 7
+// function outer() {
+//     let count = 0;
+//     return function inner() {
+//       count++;
+//       return count;
+//     };
+//   }
+//   const counter = outer();
+//   console.log(counter());
+//   console.log(counter());
+  
+// Question - 8
+// let person = { name: "Bob" };
+// Object.freeze(person);
+// person.age = 30;
+// console.log(person);
+
+// Question - 9
+// let numbers = [1, 2, 3, 4, 5];
+// let [first, ...rest] = numbers;
+// console.log(rest);
+
+// Question - 10
+// async function fetchData() {
+//     return "data";
+//   }
+//   fetchData().then(data => console.log(data));
+  
