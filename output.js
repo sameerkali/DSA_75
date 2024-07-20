c7be7ce49aed04a0d07bf507f19d1e8065f4ff4f
Sameer Faridi <s.faridi007@gmail.com>
2024-07-20 11:17:58 +0530

    output based questions: easy

---
+++ output.js
@@ -0,0 +1,41 @@
+// Question - 1
+console.log("5" + 3);
+
+
+// // Question - 2
+// const arr = [1, 2, 3];
+// arr.push(4);
+// console.log(arr);
+
+// // Question - 3
+// let a;
+// console.log(a);
+
+// // Question - 4
+// console.log(typeof null);
+
+// // Question - 5
+// let b = 2;
+// b += 3;
+// console.log(b);
+
+// // Question - 6
+// let x = 10;
+// function y() {
+//     return x;
+// }
+// console.log(y());
+
+// // Question - 7
+// const greeting = "Hello, world!";
+// console.log(greeting.length);
+
+// // Question - 8
+// console.log(2 + "2");
+
+// // Question - 9
+// let name = "Alice";
+// console.log(`Hello, ${name}!`);
+
+// // Question - 10
+// console.log([1, 2, 3].indexOf(2));
