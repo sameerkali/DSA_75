ee7b42fb896c91c11fe2597f02afd343e3db10ae
Sameer Faridi <s.faridi007@gmail.com>
2024-08-01 23:17:47 +0530

    How it work ??

--- howItWorksInsideTheMemory.js
+++ howItWorksInsideTheMemory.js
@@ -1,16 +1,16 @@
 // Q1
 
 
-// function outer() {
-//     let count = 0;
-//     return function inner() {
-//       count++;
-//       return count;
-//     };
-//   }
-//   const counter = outer();
-//   console.log(counter());
-//   console.log(counter());
+function outer() {
+    let count = 0;
+    return function inner() {
+      count++;
+      return count;
+    };
+  }
+  const counter = outer();
+  console.log(counter());
+  console.log(counter());
 
 
 
