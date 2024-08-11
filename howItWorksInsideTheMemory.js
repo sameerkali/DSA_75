f84e4b2b1f0145bb25ac5c669470ad4ced7e1d48
Sameer Faridi <s.faridi007@gmail.com>
2024-08-11 23:13:54 +0530

    closure practice

--- howItWorksInsideTheMemory.js
+++ howItWorksInsideTheMemory.js
@@ -1,17 +1,11 @@
 // Q1
 
-
 function outer() {
-    let count = 0;
-    return function inner() {
-      count++;
-      return count;
-    };
-  }
-  const counter = outer();
-  console.log(counter());
-  console.log(counter());
-
-
-
-//Q2
+  let count = 0;
+  return function inner() {
+    count++;
+    return count;
+  };
+}
+const counter = outer();
+console.log(counter());
