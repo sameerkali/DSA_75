94b3210a8f0a9c38124fba4f9940f6d1d2d8e264
Sameer Faridi <s.faridi007@gmail.com>
2024-08-16 23:57:32 +0530

    Singleton Pattern

---
+++ Patterns/singleton.js
@@ -0,0 +1,27 @@
+/*
+2. Singleton Pattern
+The Singleton Pattern restricts the instantiation of a class to one single instance. It's useful when only one instance of an object is needed throughout the lifecycle of the application.
+*/
+
+
+const Singleton = (function() {
+    let instance;
+
+    function createInstance() {
+        return new Object('I am the instance');
+    }
+
+    return {
+        getInstance: function() {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+const instance1 = Singleton.getInstance();
+const instance2 = Singleton.getInstance();
+
+console.log(instance1 === instance2); // true
