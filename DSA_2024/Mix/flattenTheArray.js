8de5fb1d7eeb18f1b61be4ed71c6fcfdf069f0ec
Sameer Faridi <s.faridi007@gmail.com>
2024-08-18 23:29:18 +0530

    understand

--- DSA_2024/Mix/flattenTheArray.js
+++ DSA_2024/Mix/flattenTheArray.js
@@ -1,16 +1,36 @@
+// const flattenArray = a => {
+//     let newA = a.flat()
+//     // console.log(newA.flat())
+
+//     for(let i = 0; i < newA.length; i++){
+//         if(typeof newA[i] === 'object'){
+//             return newA.flat()
+//             flattenArray(newA)
+//         }
+//     }
+
+//     return newA
+// };
+// const a = [1, 2, 3, [4, [5, 6, [1, [3,4]]]], 7, 8];
+// const result = flattenArray(a);
+// console.log(result);
+
+//! GPT code
+
 const flattenArray = a => {
-    let newA = a.flat()
-    // console.log(newA.flat())
+  let newA = [];
 
-    for(let i = 0; i < newA.length; i++){
-        if(typeof newA[i] === 'object'){
-            return newA.flat()
-            flattenArray(newA)
-        }
+  for (let i = 0; i < a.length; i++) {
+    if (Array.isArray(a[i])) {
+      newA = newA.concat(flattenArray(a[i]))
+    } else {
+      newA.push(a[i]);
     }
+  }
 
-    return newA
+  return newA;
 };
-const a = [1, 2, 3, [4, [5, 6, [1, [3,4]]]], 7, 8];
+
+const a = [1, 2, 3, [4, [5, 6, [1, [3, 4]]]], 7, 8, [2, [2, [2, [2]]]]];
 const result = flattenArray(a);
 console.log(result);
