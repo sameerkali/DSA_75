6ba9fb179ba8e25615751134eb3b7af2394a13ea
Sameer Faridi <76611058+sameerkali@users.noreply.github.com>
2024-08-30 23:48:28 +0530

    DSA: Flatten The Array

--- DSA_2024/Mix/flattenTheArray.js
+++ DSA_2024/Mix/flattenTheArray.js
@@ -53,3 +53,21 @@ count++;
 const a = [1,{test: 'test'}, 2, 3, [4, [5, 6, [1, [3, 4]]]], 7, 8, [2, [2, [2, [2]]]]];
 const result = flattenArray(a, []);
 console.log(result, count);
+
+
+// const flattenArray = a => {
+//     let newA = a.flat()
+//     // console.log(newA.flat())
+
+//     for(let i = 0; i < newA.length; i++){
+//         if(typeof newA[i] === 'object'){
+//             return newA.flat()
+//             flattenArray(newA)
+//         }
+//     }
+
+//     return newA
+// };
+// const a = [1, 2, 3, [4, [5, 6, [1, [3,4]]]], 7, 8];
+// const result = flattenArray(a);
+// console.log(result);
