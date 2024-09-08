4a69c234d636075ec1c960e1531fa95adc595f7f
sameerkali <s.faridi007@gmail.com>
2024-09-08 23:06:47 +0530

    Debouncing

---
+++ DSA_2024/Mix/debounding.js
@@ -0,0 +1,12 @@
+const debounce = (fn, delay) => {
+    let timeoutId;
+    return (...args) => {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(() => fn(...args), delay);
+    };
+};
+
+const add = (a, b) => console.log(a + b);
+const debouncedAdd = debounce(add, 500);
+
+debouncedAdd(2, 3);
