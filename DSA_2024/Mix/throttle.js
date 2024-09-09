532e2a134fef41400191760c6a3bcfd5b3c1c475
Sameer Faridi <76611058+sameerkali@users.noreply.github.com>
2024-09-09 23:04:34 +0530

    fix: throttle

--- DSA_2024/Mix/throttle.js
+++ DSA_2024/Mix/throttle.js
@@ -1,3 +1,21 @@
+// const throttle = (fn, delay) => {
+//     let lastCall = 0;
+//     return (...args) => {
+//         const now = new Date().getTime();
+//         if (now - lastCall >= delay) {
+//             lastCall = now;
+//             fn(...args);
+//         }
+//     };
+// };
+
+// const log = (msg) => console.log(msg);
+// const throttledLog = throttle(log, 1000);
+
+// throttledLog("Hello"); 
+
+
+
 const throttle = (fn, delay) => {
     let lastCall = 0;
     return (...args) => {
