66b099bcf8dc07cb71e3f42d0607417e7f4814b8
sameerkali <s.faridi007@gmail.com>
2024-09-10 19:14:55 +0530

    DSA: Sort

---
+++ DSA_2024/Mix/Sort.js
@@ -0,0 +1,18 @@
+
+
+
+const simpleSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+const exampleArray = [64, 34, 25, 12, 22, 11, 90];
+console.log(simpleSort(exampleArray));
