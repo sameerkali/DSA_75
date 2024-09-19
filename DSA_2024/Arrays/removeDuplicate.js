bf5b864778bac1b85117c382dbf6a8d12eb4cae1
sameerkali <s.faridi007@gmail.com>
2024-09-19 15:37:29 +0530

    practice for interview factwise

---
+++ DSA_2024/Arrays/removeDuplicate.js
@@ -0,0 +1,12 @@
+const removeDuplicate = (arr) => {
+    let container = new Set
+    for (let i = 0; i < arr.length; i++) {
+        if(!container.has(arr[[i]])){
+            container.add(arr[i])
+        }
+    }
+    return Array.from(container)
+}
+
+let arr = [1,2,3,4,5,6,7,8,1,2,3]
+console.log(removeDuplicate(arr))
