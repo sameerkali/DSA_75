53fc56b7e36c8ab23b2cd80977c5d790b46afb89
sameerkali <s.faridi007@gmail.com>
2024-12-30 18:11:10 +0530

    reverse a string

--- new_job_new_prepration_at_rishikesh/dsa/string/Palindrome.js
+++ new_job_new_prepration_at_rishikesh/dsa/string/Palindrome.js
@@ -1,16 +1,51 @@
-const palindrome = (s) => {
-if(s === " ") return true
-let ans = s.replace(' ', ''), start=0, end=ans.length, sam=''
-for(let i=0; i<end;i++){
-if(ans[start]===ans[end]){
-  sam+=ans[start]
-}else{
-  sam=''
-}
-}
-if(ans === sam) return true 
-else return false
-}
+// const palindrome = (s) => {
+//   if (s === " ") return true;
+//   let ans = s.replace(" ", ""),
+//     start = 0,
+//     end = ans.length,
+//     sam = "";
+//   for (let i = 0; i < end; i++) {
+//     if (ans[start] === ans[end]) {
+//       sam += ans[start];
+//     } else {
+//       sam = "";
+//     }
+//   }
+//   if (ans === sam) return true;
+//   else return false;
+// };
 
-let str = "race car"
-console.log(palindrome(str))
+// let str = "asasa";
+// console.log(palindrome(str));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const palindrome = (str) => {
+  
+}
--- new_job_new_prepration_at_rishikesh/dsa/string/reverse_a_string.js
+++ new_job_new_prepration_at_rishikesh/dsa/string/reverse_a_string.js
@@ -1,21 +1,31 @@
-const reverseStr = (str) => {
-    let reverse = ''
-    for(let i = str.length-1; i >= 0 ; i--) {
-        reverse += str[i]
-    }
-    return reverse;
+// const reverseStr = (str) => {
+//     let reverse = ''
+//     for(let i = str.length-1; i >= 0 ; i--) {
+//         reverse += str[i]
+//     }
+//     return reverse;
+// }
+
+
+// let str = 'I am sameer'
+// // console.log(reverseStr(str))
+
+// const reverseStr2 = (str) => {
+//     return str.split(' ').reverse().join(' ')
+// }
+// console.log(reverseStr2(str));
+
+
+
+
+
+const reverse = (str) => {
+    console.log(str)
+    if(str === "") return
+
+
+    return str.split('').reverse().join("")
+
 }
-
-
-let str = 'I am sameer'
-// console.log(reverseStr(str))
-
-const reverseStr2 = (str) => {
-    return str.split(' ').reverse().join(' ')
-}
-console.log(reverseStr2(str));
-
-
-
-
-
+let str = "sameer far89di"
+console.log(reverse(str))
