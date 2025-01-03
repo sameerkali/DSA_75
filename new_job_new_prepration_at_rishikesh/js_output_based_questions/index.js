a4855936b87e7f192a95b54541e6f38650a60e03
sameerkali <s.faridi007@gmail.com>
2025-01-03 23:44:50 +0530

    closure questions practice

---
+++ new_job_new_prepration_at_rishikesh/js_output_based_questions/index.js
@@ -0,0 +1,38 @@
+//1
+// function outer() {
+//     let count = 0;
+//     return function inner() {
+//         count++;
+//         console.log(count);
+//     };
+// }
+// const fn = outer();
+// fn();
+// fn();
+
+
+// //2
+// function createCounter() {
+//     let counter = 0;
+//     return {
+//         increment: function () {
+//             counter++;
+//             return counter;
+//         },
+//     };
+// }
+// const counter = createCounter();
+// console.log(counter.increment());
+// console.log(counter.increment());
+
+
+// 3
+function makeAdder(x) {
+    return function (y) {
+        return x + y;
+    };
+}
+const add5 = makeAdder(5);
+console.log(add5(2));
+console.log(add5(10));
+
