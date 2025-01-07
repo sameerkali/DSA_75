b61b0e2a2a774e853745783cefd8f499c715a0bf
sameerkali <s.faridi007@gmail.com>
2025-01-07 23:10:06 +0530

    searching

--- new_job_new_prepration_at_rishikesh/dsa/array/binarySearch.js
+++ new_job_new_prepration_at_rishikesh/dsa/array/binarySearch.js
@@ -1,25 +1,30 @@
 const binarySearch = (arr, target) => {
-    let left = 0;
-    let right = arr.length - 1;
-  
-    while (left <= right) {
-      const mid = Math.floor((left + right) / 2);
-  
-      if (arr[mid] === target) {
-        return mid; // Return the index if the target is found
-      } else if (arr[mid] < target) {
-        left = mid + 1; // Search the right half
-      } else {
-        right = mid - 1; // Search the left half
-      }
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
-  
-    return -1; // Return -1 if the target is not found
-  };
-  
-  // Example Usage
-  const sortedArr = [10, 20, 30, 40, 50];
-  const target = 30;
-  
-  console.log(binarySearch(sortedArr, target)); // Output: 2
-  
+  }
+
+  return -1;
+};
+
+const sortedArr = [10, 20, 30, 40, 50];
+const target = 30;
+
+console.log(binarySearch(sortedArr, target));
+
+// const search = (arr, target) =>{
+// return arr.indexOf(target)
+// }
+
+// let array = [10,20,3,4,5,6]
+// console.log(search(array, 5))
