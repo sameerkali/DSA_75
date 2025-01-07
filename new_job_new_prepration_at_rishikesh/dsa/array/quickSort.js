2c8c221c648072c9a6c1658b85ad23c35893c5e4
sameerkali <s.faridi007@gmail.com>
2025-01-07 21:49:04 +0530

    practice sorting

--- new_job_new_prepration_at_rishikesh/dsa/array/quickSort.js
+++ new_job_new_prepration_at_rishikesh/dsa/array/quickSort.js
@@ -1,20 +1,55 @@
-const quickSort = (arr) => {
-  if (arr.length <= 1) return arr;
+// const quickSort = (arr) => {
+//   if (arr.length <= 1) return arr;
 
-  const pivot = arr[arr.length - 1];
-  const left = [];
-  const right = [];
+//   const pivot = arr[arr.length - 1];
+//   const left = [];
+//   const right = [];
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] < pivot) {
+//       left.push(arr[i]);
+//     } else {
+//       right.push(arr[i]);
+//     }
+//   }
+
+//   return [...quickSort(left), pivot, ...quickSort(right)];
+// };
+
+
+
+// const quickSort = (arr) => {
+//     if(arr.length <= 1) return arr
+//     let pivot = arr[arr.length-1]    
+//     let left = []
+//     let right = []
+
+//     for (let i = 0; i < arr.length-1; i++) {
+//         if(arr[i] < pivot){
+//             left.push(arr[i])
+//         }else{
+//             right.push(arr[i])
+//         }
+        
+//     }
+
+//     return [...quickSort(left), pivot, ...quickSort(right)]
+// }
+
+
+const sort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+            let temp 
+            if(arr[i] > arr[j]){
+                temp = arr[i]
+                arr[i]= arr[j]
+                arr[j] = temp
+            }
+        }
     }
-  }
-
-  return [...quickSort(left), pivot, ...quickSort(right)];
-};
+    return arr
+}
 
 const arr = [3, 6, 8, 10, 1, 2, 1];
-console.log(quickSort(arr)); // Output: [1, 1, 2, 3, 6, 8, 10]
+console.log(sort(arr));
