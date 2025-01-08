08ab1c8fb4835342a8642dcb8550f542249b8a9b
sameerkali <s.faridi007@gmail.com>
2025-01-08 13:52:46 +0530

    pattern 01

---
+++ new_job_new_prepration_at_rishikesh/dsa/pattern/pattern.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+function rightAngledTriangle(rows) {
+    for (let i = 1; i <= rows; i++) {
+        console.log('*'.repeat(i));
+    }
+}
+rightAngledTriangle(5);
+
+
+// *
+// **
+// ***
+// ****
+// *****
+
+
