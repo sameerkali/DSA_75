a1363b804801cdefe2b8e40962080b7929f8d152
sameerkali <s.faridi007@gmail.com>
2025-01-08 13:53:25 +0530

    pattern 04

--- new_job_new_prepration_at_rishikesh/dsa/pattern/pattern.js
+++ new_job_new_prepration_at_rishikesh/dsa/pattern/pattern.js
@@ -55,3 +55,27 @@ rightAngledTriangle(5);
 //  *******
 // *********
 
+
+
+
+// function diamondPattern(rows) {
+//     for (let i = 1; i <= rows; i++) {
+//         console.log(' '.repeat(rows - i) + '*'.repeat(2 * i - 1));
+//     }
+//     for (let i = rows - 1; i >= 1; i--) {
+//         console.log(' '.repeat(rows - i) + '*'.repeat(2 * i - 1));
+//     }
+// }
+// diamondPattern(5);
+
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
