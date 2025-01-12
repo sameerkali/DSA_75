47fb74ceadee0189f85133b3f6065c669c35be91
sameerkali <s.faridi007@gmail.com>
2025-01-12 22:30:33 +0530

    PRACTICE MEMORY IN JS

--- howItWorksInsideTheMemory.js
+++ howItWorksInsideTheMemory.js
@@ -9,3 +9,26 @@ function outer() {
 }
 const counter = outer();
 console.log(counter());
+
+
+// Q-2
+// function greet(name) {
+//   return function () {
+//     console.log(`Hello, ${name}!`);
+//   };
+// }
+// const sayHello = greet("sameer");
+// sayHello();
+
+
+// Q-3
+// When passing methods as callbacks, the original context can be lost
+const person = {
+  name: "Eve",
+  greet() {
+    console.log("Hi, " + this.name);
+  }
+};
+const greetFunc = person.greet;
+// greetFunc(); // Undefined or error, because `this` is not bound to person.
+
