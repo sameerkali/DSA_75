c79c2227981d07603a41c79911ad9e58b3a10fdd
sameerkali <s.faridi007@gmail.com>
2025-01-13 18:11:51 +0530

    Promise.any

--- promise.js
+++ promise.js
@@ -9,11 +9,11 @@ const promise2 = new Promise((resolve, reject) => {
   // setTimeout(() => resolve("p2 is pass"), 6);
 });
 const promise3 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("p3 is sucess"), 2000);
+    setTimeout(() => resolve("p3 is sucess"), 3000);
   // setTimeout(() => reject("p3 is fail"), 200);
 });
 const promise4 = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("p4 is sucess"), 2000);
+    setTimeout(() => resolve("p4 is sucess"), 4000);
   // setTimeout(() => reject("p4 is fail"), 30);
 });
 const promise5 = new Promise((resolve, reject) => {
@@ -21,7 +21,7 @@ const promise5 = new Promise((resolve, reject) => {
   // setTimeout(() => resolve("p5 is pass"), 100);
 });
 const promise6 = new Promise((resolve, reject) => {
-    setTimeout(() => reject("p6 is failed"), 2000);
+    setTimeout(() => reject("p6 is failed"), 200);
   // setTimeout(() => reject("p6 is fail"), 1100);
 });
 
@@ -38,13 +38,13 @@ const promise6 = new Promise((resolve, reject) => {
 
 //* promise.allSetteld()
 
-Promise.allSettled([promise1, promise2, promise3,promise4, promise5, promise6])
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// Promise.allSettled([promise1, promise2, promise3,promise4, promise5, promise6])
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 /* tell the status of all after executing all */
 
 //* promise.race()
@@ -60,13 +60,13 @@ Promise.allSettled([promise1, promise2, promise3,promise4, promise5, promise6])
 
 //* promise.any()
 
-// Promise.any([promise1, promise2, promise3,promise4, promise5, promise6])
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+Promise.any([promise1, promise2, promise3,promise4, promise5, promise6])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 /* return first sucess promise */
 
