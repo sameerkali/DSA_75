a8822446344135de4cb185bd049f2beb6350a51e
sameerkali <s.faridi007@gmail.com>
2025-01-13 18:41:58 +0530

    type coercion problems

---
+++ typeCoercion.js
@@ -0,0 +1,83 @@
+// console.log(2 == "2");  // true, because "2" is coerced to number 2
+// console.log(2 === "2"); // false, because the types are different
+
+
+
+
+// console.log("4" - 2);    // 2, "4" is coerced to number 4
+// console.log("4" + 2);    // "42", the + operator treats one operand as a string and performs concatenation
+// console.log(true + 1);   // 2, true is coerced to 1
+// console.log(false - 1);  // -1, false is coerced to 0
+
+
+
+
+
+
+// if (0) {
+//     console.log("This won't print because 0 is falsy");
+//   }
+  
+//   if ("") {
+//     console.log("This won't print because empty string is falsy");
+//   }
+  
+//   // Falsy values include: 0, "", null, undefined, NaN, and false.
+
+  
+
+
+
+
+
+
+
+//   console.log(null == undefined);  // true
+// console.log(null === undefined); // false
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(Number("123"));      // ?
+// console.log(String(123));        // ?
+// console.log(Boolean(0));         // ?
+// console.log(Boolean("hello"));   // ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log([] + []);        // ?
+// console.log({} + []);        // ?
+// console.log([] + {});        // ?
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("5" * "2");  // 10 because both strings are coerced to numbers
+// console.log(Number("5") * Number("2"));  // 10
