5d55a0af22a4d9238b63252850f4f08f9bde0537
sameerkali <s.faridi007@gmail.com>
2025-02-13 22:59:20 +0530

    simple Sort

--- DSA_2024/Mix/Sort.js
+++ DSA_2024/Mix/Sort.js
@@ -14,16 +14,14 @@
 // const exampleArray = [64, 34, 25, 12, 22, 11, 90];
 // console.log(simpleSort(exampleArray));
 
-const simpleSort = arr => {
+const simpleSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-      let temp;
-      if(arr[i]>arr[j]){
-        temp = arr[i]
-        arr[i]=arr[j]
-        arr[j]=temp
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
       }
-
     }
   }
   return arr
