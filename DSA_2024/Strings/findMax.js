bcd59c84117346a18283c3dc84b68773c54bb46a
sameerkali <s.faridi007@gmail.com>
2025-02-13 22:36:34 +0530

    find Max

---
+++ DSA_2024/Strings/findMax.js
@@ -0,0 +1,12 @@
+
+const findMax = (arr) => {
+    let max = arr[0];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) max = arr[i];
+    }
+  
+    return max;
+  };
+  
+  console.log(findMax([1, 2, 3, 4, 5, 6, 56, 2, 3, 4]));
--- DSA_2024/Strings/palindrome.js
+++ DSA_2024/Strings/palindrome.js
@@ -18,3 +18,4 @@ function isPalindrome(str) {
 }
 
 console.log(isPalindrome("racecar"));
+
