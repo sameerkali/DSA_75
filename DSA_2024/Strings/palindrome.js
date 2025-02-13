29b1292118ad0760b780efd326052c320a9365a7
sameerkali <s.faridi007@gmail.com>
2025-02-13 22:27:54 +0530

    is Palindrome

--- DSA_2024/Strings/palindrome.js
+++ DSA_2024/Strings/palindrome.js
@@ -1,7 +1,20 @@
-const isPalindrome = (w) => {
-let reverse = w.split('').reverse().join('');
-let forword = w.split('').join('')
-return reverse === forword
+// const isPalindrome = (w) => {
+// let reverse = w.split('').reverse().join('');
+// let forword = w.split('').join('')
+// return reverse === forword
+// }
+
+// console.log(isPalindrome("samas"))
+
+function isPalindrome(str) {
+  let left = 0,
+    right = str.length - 1;
+  while (left < right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
 }
 
-console.log(isPalindrome("samas"))
+console.log(isPalindrome("racecar"));
