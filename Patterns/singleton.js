1972083a47125294b374f62483ddab7ac6b1d7e0
Sameer Faridi <76611058+sameerkali@users.noreply.github.com>
2025-02-20 23:32:46 +0530

    revise singleton pattern

--- Patterns/singleton.js
+++ Patterns/singleton.js
@@ -6,6 +6,30 @@ the application.
 */
 
 
+// const Singleton = (function() {
+//     let instance;
+
+//     function createInstance() {
+//         return new Object('I am the instance');
+//     }
+
+//     return {
+//         getInstance: function() {
+//             if (!instance) {
+//                 instance = createInstance();
+//             }
+//             return instance;
+//         }
+//     };
+// })();
+
+// const instance1 = Singleton.getInstance();
+// const instance2 = Singleton.getInstance();
+
+// console.log(instance1 === instance2); // true
+
+
+
 const Singleton = (function() {
     let instance;
 
@@ -27,3 +51,5 @@ const instance1 = Singleton.getInstance();
 const instance2 = Singleton.getInstance();
 
 console.log(instance1 === instance2); // true
+
+
