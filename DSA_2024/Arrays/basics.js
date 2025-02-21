d4dbe7b0934eec5f7d00246eb730ff4045e25fd2
sameerkali <s.faridi007@gmail.com>
2025-02-21 23:42:24 +0530

    array recall practice

--- DSA_2024/Arrays/basics.js
+++ DSA_2024/Arrays/basics.js
@@ -8,9 +8,9 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-myArr.push(6)
-myArr.push(7)
-myArr.pop()
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
 
 myArr.unshift(9)
 // myArr.shift()
