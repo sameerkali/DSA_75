71cc4bd9b58dd12be746c79344e151134604c860
Sameer Faridi <76611058+sameerkali@users.noreply.github.com>
2025-04-27 22:59:58 +0530

    B2AHD

--- js_questions_piyushGarg/questions.js
+++ js_questions_piyushGarg/questions.js
@@ -31,7 +31,7 @@ console.log("1")
 console.log("2")
 console.log("3")
 console.log("4")
-console.log("5")
+//console.log("5")
 setTimeout(()=> console.log(6), 0)
 Promise.resolve().then(console.log("7"));
 // Promise.resolve(7).then(console.log);
