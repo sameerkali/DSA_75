02a61792db4f41a03d40e42d7ddc039532b0a97b
Sameer Faridi <76611058+sameerkali@users.noreply.github.com>
2025-07-07 18:41:48 +0530

    Check Contains Duplicate 2

--- new_job_new_prepration_at_rishikesh/dsa/sliding window/ContainsDuplicate2.js
+++ new_job_new_prepration_at_rishikesh/dsa/sliding window/ContainsDuplicate2.js
@@ -1,3 +1,21 @@
+// var containsNearbyDuplicate = function(nums, k) {
+//     const seen = {};
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const val = nums[i];
+//         if (seen.hasOwnProperty(val) && i - seen[val] <= k) {
+//             return true;
+//         }
+//         seen[val] = i;
+//     }
+
+//     return false;
+// };
+
+
+
+//! sol 2
+
 var containsNearbyDuplicate = function(nums, k) {
     const seen = {};
 
